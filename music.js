c45fe9763be6cf3eae0e0f85dacf6994c1cbba39
viratKohliKin <147699210+viratKohliKin@users.noreply.github.com>
2023-10-12 19:13:28 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,12 @@
+function playAudio() {
+      const audio1 = document.getElementById('birthdayAudio1');
+
+      audio1.play().catch(error => console.error('Error playing audio 1:', error));
+    }
+
+    // Call the playAudio function when the user scrolls
+    window.onscroll = function() {
+      playAudio();
+      // Remove the event listener to ensure audio is only played once
+      window.onscroll = null;
+    };
